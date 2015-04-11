1c7272d905a07937652974628a6ecf457ac60189
ethan <ethan@dyn-129-236-208-251.dyn.columbia.edu>
2015-04-11 17:28:06 -0400

    Initial Commit

---
+++ MusicGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicGame
+//
+//  Created by ethan on 4/11/15.
+//  Copyright (c) 2015 Ethan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicGame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  MusicGame
+//
+
+//  Copyright (c) 2015 Ethan. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
